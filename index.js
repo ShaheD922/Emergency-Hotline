f3b38cf27a79d1e361c30bf63ea3a988fa93b79b
ShaheD922 <shahedhasan.yc@gmail.com>
2025-08-29 16:58:21 +0600

    added alert on copy button

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// helper function
+// my  function
 
-function getElement(id){
-    return document.getElementById(id);
+function getElement(id)
+{
+  return document.getElementById(id);
 }
 
 
-function getInnerText(id){
-    return parseInt(getElement(id).innerText);
+function getInnerText(id)
+{
+ return parseInt(getElement(id).innerText);
 }
 
 
 
 
 // my heart
-getElement("heart").addEventListener("click",function(){
+getElement("heart").addEventListener("click",function()
+
+{
 
 getElement("heart-Count").innerText = getInnerText("heart-Count")+1;
 
@@ -21,8 +25,7 @@ getElement("heart-Count").innerText = getInnerText("heart-Count")+1;
 
 const heartIcons =document.querySelectorAll(".card .fa-heart");
 
-for (let i = 0; i < heartIcons.length; i++) 
-    
+for (let i=0;i<heartIcons.length;i++)   
 {
   heartIcons[i].addEventListener("click", function () 
   {
@@ -34,29 +37,31 @@ for (let i = 0; i < heartIcons.length; i++)
   });
 }
 
-
-// Copy buttons)
 const copyButtons = document.querySelectorAll(".card .btn");
 
-for (let i = 0; i < copyButtons.length; i++) 
+for (let i = 0; i < copyButtons.length; i++) {
+  if (copyButtons[i].querySelector(".fa-copy")) {
+    copyButtons[i].addEventListener("click", function () 
     {
- 
-  if (copyButtons[i].querySelector(".fa-copy")) 
-    {
-    copyButtons[i].addEventListener("click", function() 
-    {
-      const copyCount = document.getElementById("copy-Count");
-      copyCount.innerText = parseInt(copyCount.innerText) + 1;
+      const card = this.parentNode.parentNode;
+      const number= card.querySelector(".num-btn").innerText;
+      navigator.clipboard.writeText(number);
+
+     
+      const copyCount =document.getElementById("copy-Count");
+      copyCount.innerText =parseInt(copyCount.innerText) + 1;
+
+     
+      alert("Copied: " + number);
     });
   }
 }
 
-// call
 const callButtons = document.querySelectorAll(".card .call-btn");
 
-for (let i = 0; i < callButtons.length; i++) 
+for (let i=0;i<callButtons.length;i++) 
     
-    {
+  {
   callButtons[i].addEventListener("click", function () 
   {
    
@@ -64,8 +69,9 @@ for (let i = 0; i < callButtons.length; i++)
     const serviceName = card.querySelector(".font-bold").innerText;
     const serviceNumber = card.querySelector(".text-2xl").innerText;
 
-    if (getInnerText("coin-Count") < 20) {
-      alert("Not enough coins to call!");
+    if (getInnerText("coin-Count")<20) 
+    {
+      alert("Not enough coins to call");
       return;
     }
 
@@ -83,15 +89,21 @@ for (let i = 0; i < callButtons.length; i++)
         <div class="text-xl font-bold">${serviceName}</div>
         <div class="text-gray-500 text-xl">${serviceNumber}</div>
       </div>
-      <div class="text-gray-500 text-xl font-sm">${now.toLocaleTimeString()}</div>
+      <div class="text-gray-500 text-xl font-sm">${now.toLocaleTimeString()}
+      </div>
     `;
 
     historyList.appendChild(newHistory);
   });
+
+
+
+
 }
 
 
 
-getElement("copy-btn").addEventListener("click", function () {
+getElement("copy-btn").addEventListener("click", function () 
+{
   getElement("historyList").innerHTML = "";
 });
